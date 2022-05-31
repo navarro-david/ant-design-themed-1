bba493e7a46c9b76109b36cd40f62becfbf54651
David Navarro <navarro.david.j@gmail.com>
2022-05-31 13:18:50 -0700

    Clean up for Button.stories.jsx

--- src/stories/Button.stories.jsx
+++ src/stories/Button.stories.jsx
@@ -1,49 +1,23 @@
 import React from 'react';
 import {
-  Button as AntButton,
+  Button,
 } from 'antd';
-
-import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
-  component: AntButton,
+  component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template = (args) => <AntButton {...args}> Ant Button </AntButton>;
+const Template = (args) => <Button {...args}> Ant Button </Button>;
 
-// export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-// Primary.args = {
-//   primary: true,
-//   label: 'Button',
-// };
 
-// export const Secondary = Template.bind({});
-// Secondary.args = {
-//   label: 'Button',
-// };
-
-// export const Large = Template.bind({});
-// Large.args = {
-//   size: 'large',
-//   label: 'Button',
-// };
-
-// export const Small = Template.bind({});
-// Small.args = {
-//   size: 'small',
-//   label: 'Button',
-// };
-
-export const AntBtn = Template.bind({});
-AntBtn.args = {
+export const Primary = Template.bind({});
+Primary.args = {
   type: 'primary',
-  child: 'Ant Button',
 };
+
+
